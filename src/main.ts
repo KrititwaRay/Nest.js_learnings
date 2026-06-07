4bf0c94ba0f42aa9a0c670338eadec2f554cdf49
krititwa <krititwaray97@gmail.com>
2026-06-07 19:11:53 +0530

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // =========================
+  // USER API DOCS
+  // =========================
+
+  const userConfig = new DocumentBuilder()
+    .setTitle('User API')
+    .setDescription('All user-related APIs')
+    .setVersion('1.0')
+    .build();
+
+  const userDocument = SwaggerModule.createDocument(app, userConfig, {
+    include: [], // optionally include UserModule here
+  });
+  SwaggerModule.setup('api/user', app, userDocument);
+
+
+
+  // =========================
+  // ADMIN API DOCS
+  // =========================
+  const adminConfig = new DocumentBuilder()
+    .setTitle('Admin API')
+    .setDescription('All admin-related APIs')
+    .setVersion('1.0')
+    .build();
+
+  const adminDocument = SwaggerModule.createDocument(app, adminConfig, {
+    include: [], // optionally include AdminModule here
+  });
+
+  SwaggerModule.setup('api/admin', app, adminDocument);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { UserService } from './user.service';
-
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+@ApiTags('Users')
 @Controller('user')
 export class UserController {
     constructor(
@@ -10,11 +11,12 @@ export class UserController {
 
 
     @Get(':id')
-    getUser(@Param('id', ParseIntPipe ) id: number) {
+    getUser(@Param('id', ParseIntPipe) id: number) {
         return this.userService.getUserById(id);
     }
 
     @Get()
+    @ApiOperation({ summary: 'Get all users' })
     getAllUser() {
         return this.userService.getAllUsers();
     }
